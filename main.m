540cf0c40c318e3c93e439a4372aefa1b049a7d6
Jorge Rodríguez Peña <71644318+jorgerodrguezpea@users.noreply.github.com>
2020-11-03 14:03:03 +0000

    Updated main added column names file

--- main.m
+++ main.m
@@ -1,34 +1,33 @@
 clear all; close all; clc
-all_columns = ["ID", 
-    'Age',
-    'Gender', 
-    'Education', 
-    'Country',
-    'Ethnicity',
-    'Neuroticism',
-    'Extraversion',
-    'Openness to experience',
-    'Agreeableness',
-    'Conscientiousness',
-    'Impulsiveness',
-    'Sensation seeking',
-    'Alcohol consumption',
-    'Amphetamines consumption',
-    'Amyl nitrite consumption',
-    'Benzodiazepine consumption',
-    'Caffeine consumption',
-    'Cannabis consumption',
-    'Chocolate consumption',
-    'Cocaine consumption',
-    'Crack consumption',
-    'Ecstasy consumption',
-    'Heroin consumption',
-    'Ketamine consumption',
-    'Legal highs consumption',
-    'Lysergic acid diethylamide consumption',
-    'Methadone consumption',
-    'Magic mushrooms consumption',
-    'Nicotine consumption',
-    'Fictitious drug Semeron consumption',
-    'Volatile substance abuse consumption'];
-A = importdata("drug_consumption.data")
+
+% Importing the file
+filename = "drug_consumption.data";
+A = importdata(filename);
+cols = importdata('columnnames.txt');
+
+% Reding the file into a dataframe
+nrows = length(A);
+ncols = length(split(cell2mat(A(1)),','));
+
+% Check if rows have different number of columns
+for i=2:nrows;
+    if length(strsplit(cell2mat(A(i)),','))~=ncols;
+        emsg = "Some rows do not have the same number of columns.";
+        emsg = emsg + "\n";
+        emsg = emsg + "Unable to read the file.";
+        emsg = compose(emsg)
+        error(emsg)
+    end
+end
+
+% Creating the cell array structure
+B = cell(nrows, ncols);
+for i=1:nrows;
+    row = split(cell2mat(A(i)),',');
+    for j=1:ncols;
+        B(i,j)=row(j);
+    end
+end
+
+% Converting to a dataset
+dataset = cell2table(B,'VariableNames',cols);
